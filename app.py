4cb40897f908d4e70441c2aa5c3e3f025b61f2c4
Jannatul Ferdous <110571832+nori0227@users.noreply.github.com>
2022-12-01 00:32:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello world, WWW!'
+    return 'Hello world, welcome to my nori0227.tech page!'
 
 
 ## Note, if you want to run this app with the command `python app.py`, you need to add the following line to the end of the file
@@ -12,10 +12,3 @@ def hello_world():
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
 
-##Notes: 
-# - The `debug=True` is used to enable debug mode. This means you have HOT-RELOADING enabled.
-    # the hot reload though does not work with the port number changes, you need to restart the server.
-# - The `host=' is used to set the host to listen on, use 0.0.0.0 or localhost to listen on all interfaces.
-    # this mean you can open up your browser an go to either `localhost` as the website, or `127.0.0.1` as the website.
-# - The `port=80` is used to set the port to listen on. you can change this to whatever open port you want.
-    # if you change the port, you need to update your website address to 0.0.0.0:8080 or localhost:8080 as an example 
